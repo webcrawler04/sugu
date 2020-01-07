3d2e9b623fcd7e0024eae773efdf9fb1e52373de
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2020-01-07 13:34:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NjY0MTA5Nzg2Nzk0ODE5NjA0.XhS4fw.ElhnJU-vMD05ZpDHMmrXl-vyCsM';
 
 exports.PREFIX = '!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyAeCzDP2UZ7uw9eB5GltPnFJfZyWCUbrBY';
