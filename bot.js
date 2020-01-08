40a16f51895fa413b8a5f202d2e5b26beff81650
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2020-01-08 11:15:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,34 +6,34 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'wintie woo') {
+    if (message.content === '!wintie woo') {
     	message.reply('joshy woo');
   	}
 });
 
 
 client.on('message', message => {
-    if (message.content === 'joshywoo') {
+    if (message.content === '!joshy woo') {
     	message.reply('wintie woo');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'fake josh') {
+    if (message.content === '!fake josh') {
     	message.reply('Fake josh is just a fake josh there is no point in him even being here why he is here I dont know');
   	}
 });
 
 
 client.on('message', message => {
-    if (message.content === 'grayson') {
-    	message.reply('A rare boy, with the ability to love with all his heart. He is sensitive, and he will give everything he has to the one he loves. He can be moody at times, and he doesnt like to open up unless he feels absolutely comfortable. He is citrus-scented. He is usually very athletic, but can get frustrated easily. He is caring, loving, and is in tune with how others are feeling. Graysons usually like to help others, and sometimes Graysons give more of themselves than they should.');
+    if (message.content === '!grayson') { 
+    	message.reply('A rare boy, with the ability to love with all his heart. He is sensitive, and he will give everything he has to the one he loves. He can be moody at times, and he doesnt like to open up unless he feels absolutely comfortable. He is citrus-scented. Can get frustrated easily. He is caring, loving, and is in tune with how others are feeling. Graysons usually like to help others, and sometimes Graysons give more of themselves than they should.');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'commands') {
-    	message.reply('1. wintie woo , 2. joshy woo,3. grayson,4. fake josh');
+    if (message.content === '!commands') {
+    	message.reply('1. wintie woo , 2. joshy woo , 3. grayson , 4.fake josh');
   	}
 });
 
