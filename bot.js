58887434879f0f577c8b0df85c132ac38e7e7981
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2020-01-09 09:00:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,37 @@ client.on('message', message => {
   	}
 });
 
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  try {
+    if (!message.mentions.members.first()) return message.reply('You need to mention the user to slap!');
+    
+    let slap = await client.nekoslife.sfw.slap();
+    
+    let embed = new Discord.RichEmbed()
+    .setTitle(message.author.username + ' slapped ' + message.mentions.members.first().user.username)
+    .setImage(slap.url)
+    .setColor('#363942');
+
+    message.channel.send(embed);
+  } catch (err) {
+    message.channel.send('There was an error!\n' + err).catch();
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  aliases: [],
+  guildOnly: false,
+  permLevel: 'User'
+};
+
+exports.help = {
+  name: 'slap',
+  category: 'Weeb',
+  description: 'Returns a slap',
+  usage: 'slap <user>'
+};
+
 
 client.on('message', message => {
     if (message.content === '!webby woo') {
